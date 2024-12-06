a9776fb66af1598d8203a8e411936b3127ed45d0
Agus Heryanto <agusheryantodev@gmail.com>
2024-12-06 19:52:42 +0700

    docs: update dummy data

--- src/seeders/data.js
+++ src/seeders/data.js
@@ -366,11 +366,12 @@ const items = [
     deleted_at: null,
   },
   {
-    name: "Ular Welang Peliharaan",
-    description: "Ular ini gak berbisa",
+    name: "Kucing Putih",
+    description:
+      "Nemu kucing ini di rumah, udah 3 harian. Kalo pemiliknya gak ketemu, mau dijual aja soalnya mukanya ngeselin",
     matched_status: false,
     images: [
-      "https://images.pexels.com/photos/34426/snake-rainbow-boa-reptile-scale.jpg?cs=srgb&dl=pexels-pixabay-34426.jpg&fm=jpg",
+      "https://www.rukita.co/stories/wp-content/uploads/2023/10/Kucing_Persia_Flatnose-2021_10_07-10_34_36_7d7e78c999ca11ba390a8a9409a0bfd3-1.webp",
     ],
     approved: true,
     type: "found",
